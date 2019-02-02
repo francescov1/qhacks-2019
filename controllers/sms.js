bd1a9b0399db15ddecd7e234c4d8d568bc37e86a
Francesco Virga <francescod.virga@gmail.com>
2019-02-01 21:41:38 -0500

    Lt

--- controllers/sms.js
+++ controllers/sms.js
@@ -4,12 +4,14 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 module.exports = {
 
   receiveSms: function(req, res, next) {
+
     const twiml = new MessagingResponse();
 
     twiml.message('Fuck bitch shit');
 
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
+    
   }
 
 }
