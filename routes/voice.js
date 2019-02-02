862c3a584d64f9afe0c0985d13a7a883ab1a1742
Francesco Virga <francescod.virga@gmail.com>
2019-02-02 11:20:37 -0500

    Rename vars

--- routes/voice.js
+++ routes/voice.js
@@ -3,6 +3,7 @@ const express = require('express');
 const controller = require('../controllers/voice');
 const router = express.Router();
 
-router.post('/', controller.postVoice);
+router.post('/initialCallHandler', controller.initialCallHandler);
+router.post('/confirmResponseToOperator', controller.confirmResponseToOperator);
 
 module.exports = router;
