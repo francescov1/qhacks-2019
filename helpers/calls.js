fa1e335b4240222b6acaaf62d26a3a5ce9b0ee49
Francesco Virga <francescod.virga@gmail.com>
2019-02-02 11:37:40 -0500

    Add querystring

--- helpers/calls.js
+++ helpers/calls.js
@@ -1,14 +1,14 @@
 'use strict';
 const config = require('../config');
 const client = require('twilio')(config.twilio.account_sid, config.twilio.auth_token);
+const querystring = require("querystring");
 
 // TODO: use node package to urlify
 module.exports = {
   initCall: function(smsReceived, fromNumber) {
 
-    while (smsReceived.includes(' '))
-      smsReceived = smsReceived.replace(' ', '%20');
-
+    smsReceived = querystring.stringify({ query: smsReceived });
+    
     return client.calls.create({
       url: config.base_url + `/api/voice/initialCallHandler?phrase=${smsReceived}&from=${fromNumber}`,
       to: config.emergency_number,
